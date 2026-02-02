a83e61ce1a3d72d0b6022ce12cc8e65a5078e106
Medval21026 <21026@supnum.mr>
2026-02-02 12:44:25 +0100

    deployer

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/api' // IP locale pour Samsung Android/PWA
+  apiUrl: 'http://172.20.10.2:8080/api'// IP locale pour Samsung Android/PWA
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api'
+  apiUrl: 'http://172.20.10.2:8080/api'
 };
