cd2304bb5e373acb0903a17dafe3ef9d23389118
Medval21026 <21026@supnum.mr>
2026-02-02 21:25:52 +0100

    deployer1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://172.20.10.2:8080/api'// IP locale pour Samsung Android/PWA
+  apiUrl: 'https://matchiservicesspring-production.up.railway.app/api'
 };
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://172.20.10.2:8080/api'
+  apiUrl: 'https://matchiservicesspring-production.up.railway.app/api'
 };
