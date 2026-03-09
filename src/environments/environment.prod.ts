5b701453e2b869bb8fe596c8844ec081b5ad19d2
Medval21026 <21026@supnum.mr>
2026-03-09 14:02:16 +0100

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // apiUrl: 'https://matchiservicesspring-production.up.railway.app/api'
-  apiUrl: 'http://localhost:8081/api'
+  apiUrl: 'http://187.124.35.219:8085/api'
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // apiUrl: 'https://matchiservicesspring-production.up.railway.app/api'
-  apiUrl: 'http://localhost:8081/api'
+  apiUrl: 'http://187.124.35.219:8085/api'
 };
