6bddc5c54779ab06cd9af3be475695b5439c2004
Medval21026 <21026@supnum.mr>
2026-03-10 12:56:17 +0100

    app ios

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.matchi.service',
+  appName: 'matchiService',
+  webDir: 'dist/matchi_service_angular/browser'
+};
+
+export default config;
